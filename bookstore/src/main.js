6268fa75d4255a4af2829e30d7c7d41a641802df
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-03-15 18:20:20 +0700

    Update main.js
    - apply bootstrap icons for project

--- bookstore/src/main.js
+++ bookstore/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import VueSlickCarousel from 'vue-slick-carousel' 
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css' 
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
