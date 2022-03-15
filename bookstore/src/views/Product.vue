23bc9b8191211c6ef90276aa133218ff0c530612
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-03-15 18:19:05 +0700

    Create Product.vue
    - test param from main page

---
+++ bookstore/src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container p-2">
+    <p>{{this.product}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Product",
+  components: {
+
+  },
+  data() {
+    return {
+        product: this.$route.params.id,
+    };
+  },
+};
+</script>
