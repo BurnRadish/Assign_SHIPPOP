e70d52fc895031ffce0cbcc9c5d17bdd9a68606d
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-03-16 14:09:51 +0700

    Update Navbar.vue
    - Add search toggle
    - Add cart icon
    - Add user icon

--- bookstore/src/components/Navbar.vue
+++ bookstore/src/components/Navbar.vue
@@ -22,8 +22,28 @@
         </li>
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+         <div class="input-group" v-if="searchToggle">
+                <input class="form-control border-end-0 border" type="search" value="search" id="example-search-input" style="border-top-left-radius: 25px; border-bottom-left-radius: 25px;">
+                <span class="input-group-append">
+                    <button class="btn btn-outline bg-white border-start-0 border-bottom border" type="button" style="height : 100%; border-top-right-radius: 25px; border-bottom-right-radius: 25px;">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </span>
+            </div>
+        <button class="btn btn-outline" @click="searchToggle = !searchToggle" v-if="searchToggle == false"><i class="bi bi-search"></i></button>
+        <button class="btn btn-outline" @click="searchToggle = !searchToggle" v-if="searchToggle"><i class="bi bi-x-lg"></i></button>
+        <button class="nav-item dropdown btn btn-outline">
+          <div class="nav-link dropdown-toggle btn btn-outline" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-cart"></i>
+          </div>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </button>
+        <img src="https://placekitten.com/g/35/35" class="d-inline-block align-top circle">
       </form>
     </div>
   </div>
@@ -34,11 +54,30 @@
 
 export default {
   name: "Navbar",
+  data(){
+    return{
+      searchToggle : false,
+    }
+  }
 };
 </script>
 
 <style scoped>
+.dropdown-toggle::after {
+    content: none;
+}
+
+
+.btn:focus {
+  outline: none !important;
+}
+
 .navB{
   font-weight: "bold";
+}
+
+.circle{
+  border-radius: 50%;
+  margin: 10px;
 }
 </style>
