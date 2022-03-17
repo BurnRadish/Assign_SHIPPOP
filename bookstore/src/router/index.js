e4ef47a50fa06805043ce5de4b8ea9a5696ceae8
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-03-17 22:31:23 +0700

    Update index.js
    - Change format call component

--- bookstore/src/router/index.js
+++ bookstore/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,12 +7,17 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import("../views/Home.vue"),
   },
   {
     path: "/product/:id",
     name: "Product",
     component: () => import("../views/Product.vue"),
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: () => import("../views/Cart.vue"),
   },
 ];
 
